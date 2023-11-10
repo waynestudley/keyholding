e4431df83b841fb01c28b2efb442dfd913e7a138
Wayne <ws@healthxchange.com>
2023-11-10 10:09:16 +0100

    wip

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,11 @@
 import type { AppProps } from "next/app";
-import { Provider } from "react-redux";
-import { store } from "../redux/store";
+
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
+    <>
       <Component {...pageProps} />
-    </Provider>
+    </>
   );
 }
 
